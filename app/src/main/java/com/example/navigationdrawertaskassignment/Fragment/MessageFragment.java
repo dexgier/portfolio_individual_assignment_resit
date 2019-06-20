a2fbe1de6bdee6c8dd31f89b11216a9ca1ba4619
Robbie Gier <gierr@hva.nl>
2019-06-20 14:24:36 +0200

    Added upload screen

--- app/src/main/java/com/example/navigationdrawertaskassignment/Fragment/MessageFragment.java
+++ app/src/main/java/com/example/navigationdrawertaskassignment/Fragment/MessageFragment.java
@@ -1,5 +1,8 @@
 package com.example.navigationdrawertaskassignment.Fragment;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,14 +10,80 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import com.example.navigationdrawertaskassignment.R;
+import com.squareup.picasso.Picasso;
+
+import static android.app.Activity.RESULT_OK;
 
 public class MessageFragment extends Fragment {
+
+
+    private static final int PICK_IMAGE_REQUEST = 1;
+    private Button mButtonChooseImage;
+    private Button mButtonUpload;
+    private EditText mEditTextFileName;
+    private ImageView mImageView;
+    private ProgressBar mProgressBar;
+    private Uri mImageUri;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_message, container, false);
+        View view = inflater.inflate(R.layout.fragment_message,container,false);
+        mButtonChooseImage = view.findViewById(R.id.button_choose_image);
+        mButtonUpload = view.findViewById(R.id.button_upload);
+        initButtons();
+
+
+
+        mEditTextFileName = view.findViewById(R.id.edit_text_file_name);
+        mImageView = view.findViewById(R.id.image_view);
+        mProgressBar = view.findViewById(R.id.progress_bar);
+
+
+        return view;
     }
+
+    private void openFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null) {
+            mImageUri = data.getData();
+
+            Picasso.with(getActivity().getApplicationContext()).load(mImageUri).into(mImageView);
+        }
+    }
+
+    private void initButtons() {
+        mButtonChooseImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openFileChooser();
+            }
+        });
+
+        mButtonUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+    }
+
 }
